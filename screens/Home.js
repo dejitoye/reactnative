b2e0fd112e27351df8c3511c7fca8cf06c749c7b
dejitoye <dejitoye71@gmail.com>
2021-10-24 21:20:44 -0500

    2nd commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({navigation}) {
+    // console.log("navigation",props)r
+    const pressH = ()=>{
+navigation.navigate("reviews")
+    }
     return (
         <View style={globalStyles.container}>
            <Text style={globalStyles.titleText}> Home screeen </Text> 
+          <Button title="go to review" onPress={pressH}/>
         </View>
     )
 }
--- screens/Review.js
+++ screens/Review.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
-export default function Review() {
+export default function Review({navigation}) {
+    const pressH = ()=>{
+        navigation.goBack()
+            }
     return (
         <View>
            <Text> review </Text> 
+           <Button title="go to home" onPress={pressH}/>
         </View>
     )
 }
